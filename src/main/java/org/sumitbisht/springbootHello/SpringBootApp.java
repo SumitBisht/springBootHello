6723b7505b9e1b095470f80f41383139a8eb366b
Sumit Bisht <sbisht@salmon.com>
2018-06-21 11:32:25 +0530

    added war dependency and added .gitignore

---
+++ src/main/java/org/sumitbisht/springbootHello/SpringBootApp.java
@@ -0,0 +1,7 @@
+package org.sumitbisht.springbootHello;
+
+/**
+ * Created by sbisht on 21-06-2018.
+ */
+public class SpringBootApp {
+}
---
+++ src/main/java/org/sumitbisht/web/controllers/HelloWorldController.java
@@ -0,0 +1,7 @@
+package org.sumitbisht.web.controllers;
+
+/**
+ * Created by sbisht on 21-06-2018.
+ */
+public class HelloWorldController {
+}
